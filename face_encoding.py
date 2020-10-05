d10ff37f67412a51902bd775f7e019fa6c3a2308
harichan1771 <33183398+harichan1771@users.noreply.github.com>
2020-10-05 23:27:12 +0530

    Add files via upload

---
+++ face_encoding.py
@@ -0,0 +1,34 @@
+import face_recognition
+import cv2
+import numpy as np
+#
+
+some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file("biden.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# add your dataset here with the exact location of image locations
+
+# Create arrays of known face encodings and their names
+all_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+
+known_face_names = [
+    "Barack Obama",
+    "Joe Biden"
+]
+
+
