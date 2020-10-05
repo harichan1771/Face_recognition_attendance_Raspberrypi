8f1cec899fc6ffe4b7275c1a3fd0a7cad6c9f892
harichan1771 <33183398+harichan1771@users.noreply.github.com>
2020-10-05 23:30:12 +0530

    Update create_face_encoding.py

--- create_face_encoding.py
+++ create_face_encoding.py
@@ -3,9 +3,6 @@ import cv2
 import numpy as np
 #
 
-some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
 
 # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
